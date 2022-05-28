986763691e79b174e0a1e009eaccc55f2bca78a5
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-05-28 08:58:21 +0800

    1、尝试发布

--- src/Controls/TextSwitch.xaml.cs
+++ src/Controls/TextSwitch.xaml.cs
@@ -1,19 +1,30 @@
 namespace JiuLing.Controls.Maui.Controls;
-
+/// <summary>
+/// 文本开关
+/// </summary>
 public partial class TextSwitch : ContentView
 {
+    /// <summary>
+    /// 要绑定的文本属性
+    /// </summary>
     public static readonly BindableProperty TextProperty =
         BindableProperty.Create(
             nameof(Text),
             typeof(string),
             typeof(TextSwitch),
             string.Empty);
+    /// <summary>
+    /// 文本内容
+    /// </summary>
     public string Text
     {
         get { return (string)GetValue(TextProperty); }
         set { SetValue(TextProperty, value); }
     }
 
+    /// <summary>
+    /// 是否启用
+    /// </summary>
     public static readonly BindableProperty IsToggledProperty =
         BindableProperty.Create(
             nameof(IsToggled),
@@ -22,12 +33,18 @@ public partial class TextSwitch : ContentView
             false,
             defaultBindingMode: BindingMode.TwoWay);
 
+    /// <summary>
+    /// 是否启用
+    /// </summary>
     public bool IsToggled
     {
         get { return (bool)GetValue(IsToggledProperty); }
         set { SetValue(IsToggledProperty, value); }
     }
 
+    /// <summary>
+    /// 初始化组件
+    /// </summary>
     public TextSwitch()
     {
         InitializeComponent();
--- src/MauiAppBuilderExtensions.cs
+++ src/MauiAppBuilderExtensions.cs
@@ -1,7 +1,14 @@
 ﻿namespace JiuLing.Controls.Maui;
 
+/// <summary>
+/// MauiAppBuilder 的扩展方法
+/// </summary>
 public static class MauiAppBuilderExtensions
 {
+    /// <summary>
+    /// 启用组件库
+    /// </summary>
+    /// <returns></returns>
     public static MauiAppBuilder UseBusiness(this MauiAppBuilder builder)
     {
         return builder;
