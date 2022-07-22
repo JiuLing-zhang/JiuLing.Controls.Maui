c51a914428d0184fc7e1a5cc1b1a9ad85a14f7c9
九零 <78294929+JiuLing-zhang@users.noreply.github.com>
2022-07-22 14:44:55 +0800

    1、新增高斯模糊组件

---
+++ src/Behaviors/BlurBehavior.cs
@@ -0,0 +1,23 @@
+﻿namespace JiuLing.Controls.Maui.Behaviors;
+public partial class BlurBehavior
+{
+    //Article: https://vladislavantonyuk.azurewebsites.net/articles/Creating-beautiful-image-effects-using-.NET-MAUI
+    public static readonly BindableProperty RadiusProperty = BindableProperty.Create(nameof(Radius), typeof(float), typeof(BlurBehavior), 10f, propertyChanged: OnRadiusChanged);
+
+    public float Radius
+    {
+        get => (float)GetValue(RadiusProperty);
+        set => SetValue(RadiusProperty, value);
+    }
+
+    static void OnRadiusChanged(BindableObject bindable, object oldValue, object newValue)
+    {
+        var behavior = (BlurBehavior)bindable;
+        if (behavior.imageView is null)
+        {
+            return;
+        }
+
+        SetRendererEffect(behavior.imageView, Convert.ToSingle(newValue));
+    }
+}
